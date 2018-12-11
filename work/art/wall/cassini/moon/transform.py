7d2e7ea7d5d9bc47c533ce48317bf1ee8b603b3c
Mark Opfell <mark@Marks-MBP.frontierlocal.net>
2018-12-11 05:19:23 -0800

    Read filenames from files in a folder

--- work/art/wall/cassini/moon/transform.py
+++ work/art/wall/cassini/moon/transform.py
@@ -1,3 +1,17 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3, 4])
-plt.show()
+import os
+import glob
+import skimage
+from skimage import io
+
+raw = 'raw'
+raw_format = '.png'
+cwd = os.path.dirname(os.path.realpath(__file__))
+data = os.path.join(cwd, raw)
+
+for file in os.listdir(data):
+    print(file)
+
+filename = os.path.join(skimage.data_dir, 'moon.png')
+moon = io.imread(filename)
+print(moon.shape)
+print(data)
