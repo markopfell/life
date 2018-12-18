ff5435725710228c8b90d07497a598ff1e60818b
Mark Opfell <markopfell@gmail.com>
2018-12-17 20:12:16 -0800

    Google job posting io skeleton

--- work/jobs/posts/parser.py
+++ work/jobs/posts/parser.py
@@ -1,12 +1,41 @@
+'# TODO get remote working?
 import requests
-import json
 
-job_url = 'https://www.google.com/search?q=jobs&oq=jobs&aqs=chrome..69i57j69i61l2j69i60j0l2.582j1j7&sourceid=chrome&ie=UTF-8&ibp=htl;jobs&sa=X&ved=2ahUKEwjN4qabsqjfAhWuHTQIHfCxDl4Qp4wCMAJ6BAgBECo#fpstate=tldetail&htidocid=viEGMcI6rAZUtNMYAAAAAA%3D%3D&htitab=save&htivrt=jobs'
-
+job_url = 'https://g.co/kgs/1QVgZR'
 r = requests.get(job_url)
 
-if r.status_code == 200:
-    r.text
+is_remote = False
+is_post = False
+post = False
+file_name = '0,software_engineer_rf_test.txt'
 
+if r.status_code != 200 and is_remote:
+    is_post = False
 
+elif r.status_code == 200 and is_remote:
+    post = r.text
+    is_post = True
+
+elif not is_remote:
+    post_file = open(file_name, mode='r')
+    post = post_file.read()
+    post_file.close()
+    is_post = True
+
+if is_post:
+    post = post.split('\n')
+    cleaned_post = []
+
+    for line in post:
+        cleaned_post.append(line.strip())
+
+    cleaned_post_key_words = []
+
+    for line in cleaned_post:
+        line.strip()
+
+    line = list(set(cleaned_post))
+
+    for line in cleaned_post:
+        print(line)
 
