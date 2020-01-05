2d6ca0f5606a00ce1936d40c7cd58ec51f5a4deb
Mark Opfell <markopfell@gmail.com>
2020-01-05 10:47:29 -0800

    PEP8 compliance

--- work/electrical_engineering/pyrfcomms/utils.py
+++ work/electrical_engineering/pyrfcomms/utils.py
@@ -8,7 +8,7 @@ def eirp(power_transmit, loss, gain_transmit):  # dBW, dB, dBi
     return power_transmit + loss + gain_transmit
 
 
-def g_over_t(gain_receive, noise_temperature): # dB, dBK
+def g_over_t(gain_receive, noise_temperature):  # dB, dBK
     return gain_receive - 10*math.log10(noise_temperature)
 
 
@@ -18,31 +18,33 @@ def ebn0(bit_error_rate, modulation):
     # PCM/PM/NRZ/SP-L ~= BPSK
     # https://deepspace.jpl.nasa.gov/files/phase1.pdf
 
-    if modulation == 'GMSK':
-        _ebn0 = 10 * math.log10(( ( scipy.special.erfcinv(bit_error_rate *  ( 2 )) )  ** 2 )  /  ( 0.9 ))
-    elif modulation == 'BPSK' or modulation == 'QPSK' or modulation == 'OQPSK' or modulation == 'PCM':
-        _ebn0 = 10 * math.log10(( scipy.special.erfcinv(bit_error_rate * 2) )  ** 2)
-    elif modulation == '8PSK' or modulation == '8APSK':
-        _ebn0 = 10 * math.log10(( ( scipy.special.erfcinv(bit_error_rate * math.log2(8)) )  ** 2 )  /  ( math.log2(8) * math.sin(math.pi / 8) ))
-    elif modulation == '16QAM' or modulation == '16APSK':
-        _ebn0 = 10 * math.log10(( ( scipy.special.erfcinv(bit_error_rate *  ( 8 / 3 )) )  ** 2 )  *  ( 10 / 4 ))
-        #TODO:  16APSK = Guess
-    elif modulation == '16APSK' or modulation == '16PSK':
-        _ebn0 = 10 * math.log10(( ( scipy.special.erfcinv(bit_error_rate * math.log2(16)) )  ** 2 )  /  ( math.log2(16) * math.sin(math.pi  / 16) ))
-        #TODO:  32APSK = Guess
-    elif modulation == '32APSK':
-        _ebn0 = 10 * math.log10(( ( scipy.special.erfcinv(bit_error_rate * math.log2(32)) )  ** 2 )  /  ( math.log2(32) * math.sin(math.pi  / 32) ))
-        #TODO:  64APSK = Guess
-    elif modulation == '64APSK':
-        _ebn0 = 10 * math.log10(( ( scipy.special.erfcinv(bit_error_rate * math.log2(64)) )  ** 2 )  /  ( math.log2(64) * math.sin(math.pi  / 64) ))
-        #TODO:  128APSK = Guess
-    elif modulation == '128APSK':
-        _ebn0 = 10 * math.log10(( ( scipy.special.erfcinv(bit_error_rate * math.log2(128)) )  ** 2 )  /  ( math.log2(128) * math.sin(math.pi  / 128) ))
-        #TODO:  256APSK = Guess
-    elif modulation == '256APSK':
-        _ebn0 = 10 * math.log10(( ( scipy.special.erfcinv(bit_error_rate * math.log2(256)) )  ** 2 )  /  ( math.log2(256) * math.sin(math.pi  / 256) ))
-    return _ebn0
+    _ebn0 = 0
 
+    if modulation == 'GMSK':
+        _ebn0 = 10 * math.log10(((scipy.special.erfcinv(bit_error_rate * 2)) ** 2) / 0.9)
+    elif modulation == 'BPSK' or modulation == 'QPSK' or modulation == 'OQPSK' or modulation == 'PCM':
+        _ebn0 = 10 * math.log10((scipy.special.erfcinv(bit_error_rate * 2)) ** 2)
+    elif modulation == '8PSK' or modulation == '8APSK':
+        _ebn0 = 10 * math.log10(((scipy.special.erfcinv(bit_error_rate * math.log2(8))) ** 2) / (math.log2(8) * math.sin(math.pi / 8)))
+    elif modulation == '16QAM' or modulation == '16APSK':
+        _ebn0 = 10 * math.log10(((scipy.special.erfcinv(bit_error_rate * (8 / 3))) ** 2) * (10 / 4))
+        # TODO:  16APSK = Guess
+    elif modulation == '16APSK' or modulation == '16PSK':
+        _ebn0 = 10 * math.log10(((scipy.special.erfcinv(bit_error_rate * math.log2(16))) ** 2) / (math.log2(16) * math.sin(math.pi / 16)))
+        # TODO:  32APSK = Guess
+    elif modulation == '32APSK':
+        _ebn0 = 10 * math.log10(((scipy.special.erfcinv(bit_error_rate * math.log2(32))) ** 2) / (math.log2(32) * math.sin(math.pi / 32)))
+        # TODO:  64APSK = Guess
+    elif modulation == '64APSK':
+        _ebn0 = 10 * math.log10(((scipy.special.erfcinv(bit_error_rate * math.log2(64))) ** 2) / (math.log2(64) * math.sin(math.pi / 64)))
+        # TODO:  128APSK = Guess
+    elif modulation == '128APSK':
+        _ebn0 = 10 * math.log10(((scipy.special.erfcinv(bit_error_rate * math.log2(128))) ** 2) / (math.log2(128) * math.sin(math.pi / 128)))
+        # TODO:  256APSK = Guess
+    elif modulation == '256APSK':
+        _ebn0 = 10 * math.log10(((scipy.special.erfcinv(bit_error_rate * math.log2(256))) ** 2) / (math.log2(256) * math.sin(math.pi / 256)))
+
+    return _ebn0
 
 
 def path_loss_troposphere():
@@ -54,7 +56,7 @@ def path_loss_ionosphere():
 
 
 def path_loss_free_space(distance, frequency):
-    _path_loss_free_space = 20 * math.log10(( 4 * math.pi * distance * frequency )  / scipy.constants.value(u'speed of light in vacuum'))
+    _path_loss_free_space = 20 * math.log10((4 * math.pi * distance * frequency) / scipy.constants.value(u'speed of light in vacuum'))
     return _path_loss_free_space
 
 
