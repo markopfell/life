4fb9dccaacd68bf706ae063c7e3c9c9d8436df28
Mark Opfell <markopfell@gmail.com>
2020-01-05 10:07:40 -0800

    Filled slant range function

--- work/electrical_engineering/pyrfcomms/utils.py
+++ work/electrical_engineering/pyrfcomms/utils.py
@@ -25,8 +25,12 @@ def path_loss_free_space():
     return
 
 
-def slant_range():
-    return
+def slant_range(mean_orbit_altitude, elevation_angle_degrees, object_radius):
+    mean_orbit_radius = mean_orbit_altitude + object_radius
+    elevation_angle_radians = math.radians(elevation_angle_degrees)
+    _slant_range = object_radius * ((((mean_orbit_radius ** 2 / object_radius ** 2) - (math.cos(elevation_angle_radians)) ** 2) ** 0.5) - math.sin(elevation_angle_radians))
+    return _slant_range
 
 
-
+EARTH_RADIUS = 6378*1000    
+print(slant_range(35786*1000, 90, EARTH_RADIUS))
