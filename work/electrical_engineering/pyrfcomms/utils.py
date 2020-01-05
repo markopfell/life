f2ab26cecd5e99aa59e80db8534deb718b2c753e
Mark Opfell <markopfell@gmail.com>
2020-01-05 02:47:23 -0800

    Link budget skeleton

--- work/electrical_engineering/pyrfcomms/utils.py
+++ work/electrical_engineering/pyrfcomms/utils.py
@@ -7,3 +7,26 @@ def eirp(power_transmit, loss, gain_transmit):  # dBW, dB, dBi
 
 def g_over_t(gain_receive, noise_temperature): # dB, dBK
     return gain_receive - 10*math.log10(noise_temperature)
+
+
+def ebn0():
+    return
+
+
+def path_loss_troposphere():
+    return
+
+
+def path_loss_ionosphere():
+    return
+
+
+def path_loss_free_space():
+    return
+
+
+def slant_range():
+    return
+
+
+
