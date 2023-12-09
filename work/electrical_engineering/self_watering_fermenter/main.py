9b91c3ffe20859c201f3bf7671931e44105c8e35
Mark Opfell <markopfell@gmail.com>
2023-12-09 13:52:31 -0800

    after cleanup 2

--- work/electrical_engineering/self_watering_fermenter/main.py
+++ work/electrical_engineering/self_watering_fermenter/main.py
@@ -4,11 +4,8 @@ import time
 pump = Pin(0, Pin.OUT, Pin.PULL_UP)    # create output pin on GPIO0 = Pin 1 (drag up to 3.3V with an internal resistor)
 sensor = Pin(1, Pin.IN, Pin.PULL_UP) # create input pin on GPIO1 = Pin 2
 
-pump.value(1)
-
-time.sleep(15)
-
 while True:
+    # time.sleep(1)
     if sensor.value() == 1:
         pump.value(1)
     elif sensor.value() == 0:
