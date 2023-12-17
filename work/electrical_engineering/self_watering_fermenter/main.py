831c555f938c4a34e1cda4a990ff43aeb741cd4c
Mark Opfell <markopfell@gmail.com>
2023-12-17 10:54:39 -0800

    debugging

--- work/electrical_engineering/self_watering_fermenter/main.py
+++ work/electrical_engineering/self_watering_fermenter/main.py
@@ -5,10 +5,12 @@ import time
 pump = Pin(0, Pin.OUT, Pin.PULL_UP) 
 
 # create input pin on GPIO1 = physical Pin 2 (drag up to 3.3V with an internal resistor)
-sensor = Pin(1, Pin.IN, Pin.PULL_UP) 
+sensor = Pin(1, Pin.IN, Pin.PULL_UP)
+# analog_sensor = ADC(Pin(28))
 
 while True:
     if sensor.value() == 1:
         pump.value(1)
     elif sensor.value() == 0:
         pump.value(0)
+        print(sensor.value())
