2f298644619d04b77373b0a3b4683b12924e3c2a
Mark Opfell <markopfell@gmail.com>
2023-12-23 12:49:51 -0800

    Removed internal pullup resistors and depend on external hardware

--- work/electrical_engineering/self_watering_fermenter/main.py
+++ work/electrical_engineering/self_watering_fermenter/main.py
@@ -1,16 +1,12 @@
 from machine import Pin
-import time
 
 # create output pin on GPIO0 = physical Pin 1
-pump = Pin(0, Pin.OUT, Pin.PULL_UP) 
+pump = Pin(0, Pin.OUT) 
 
-# create input pin on GPIO1 = physical Pin 2 (drag up to 3.3V with an internal resistor)
-sensor = Pin(1, Pin.IN, Pin.PULL_UP)
-# analog_sensor = ADC(Pin(28))
+# create input pin on GPIO1 = physical Pin 2
+# depending on Pin.PULL_UP resistors has been spotty at best, used external
+sensor = Pin(1, Pin.IN)
 
 while True:
     if sensor.value() == 1:
         pump.value(1)
-    elif sensor.value() == 0:
-        pump.value(0)
-        print(sensor.value())
