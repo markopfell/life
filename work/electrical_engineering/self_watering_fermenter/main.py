8cedb40eb27672131f6eec15abadc3cec89f6e63
Mark Opfell <markopfell@gmail.com>
2023-12-23 12:54:23 -0800

    Even more Notes

--- work/electrical_engineering/self_watering_fermenter/main.py
+++ work/electrical_engineering/self_watering_fermenter/main.py
@@ -1,14 +1,15 @@
 from machine import Pin
 
-# create output pin on GPIO0 = physical Pin 1
+# GPIO0 = physical Pin 1
 pump = Pin(0, Pin.OUT) 
 
-# create input pin on GPIO1 = physical Pin 2
+# GPIO1 = physical Pin 2
 # depending on Pin.PULL_UP resistors has been spotty at best, used external
 sensor = Pin(1, Pin.IN)
 
 while True:
-     # Liquid level sensor high == 1/logic level high in air
+     # Liquid level sensor high = 1 = logic level high = 5V in air
+     # Converted with a logic level shifter from 5V to 3.3V
     if sensor.value() == 1: 
-        # Gate of N-channel MOSFET switching Drain->Source to ground from float to 5V
+        # Gate of N-channel MOSFET switching Drain->Source to ground from (float to 5V)
         pump.value(1)    
