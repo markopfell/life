79e208badb87fcc1708e0d8ac86a8fee3f3bcae0
Mark Opfell <markopfell@gmail.com>
2025-09-06 08:35:40 -0700

    logic for multiple cities rev2

--- work/housing/quiet_streets.py
+++ work/housing/quiet_streets.py
@@ -263,7 +263,6 @@ def main():
 
     place_name = str(city + ', ' + state + ', ' + country)
 
-    place_name = 'Long Beach, CA, USA'
     zillow_search_url = 'https://www.zillow.com/long-beach-ca/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22isMapVisible%22%3Atrue%2C%22mapBounds%22%3A%7B%22west%22%3A-118.26751773364256%2C%22east%22%3A-118.04470126635741%2C%22south%22%3A33.68558471156971%2C%22north%22%3A33.89045561170285%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A46298%2C%22regionType%22%3A6%7D%5D%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22sqft%22%3A%7B%22min%22%3A750%7D%2C%2255plus%22%3A%7B%22value%22%3A%22e%22%7D%2C%22doz%22%3A%7B%22value%22%3A%227%22%7D%2C%22mp%22%3A%7B%22min%22%3A1600%2C%22max%22%3A3000%7D%2C%22price%22%3A%7B%22min%22%3A318207%2C%22max%22%3A596639%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%2C%22usersSearchTerm%22%3A%22Long%20Beach%20CA%22%7D'
 
     street_abbreviations_file = 'street_name_abbreviations_us.csv'
